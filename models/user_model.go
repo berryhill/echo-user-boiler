fd0f1a117c519fe3566c043fed7dcdde45310627
Berry <matthewberryhill@gmail.com>
2016-10-17 19:03:34 -0600

    clean up

--- models/user_model.go
+++ models/user_model.go
@@ -9,8 +9,8 @@ import (
 
 type User struct {
 	//BaseModel
-	Id 		bson.ObjectId          `json:"id",bson:"_id,omitempty"`
-	Timestamp 	time.Time	       `json:"time",bson:"time,omitempty"`
+	Id 		bson.ObjectId    `json:"id",bson:"_id,omitempty"`
+	Timestamp 	time.Time	 `json:"time",bson:"time,omitempty"`
 	Username	string           `json:"username",bson:"username,omitempty"`
 	Password	string           `json:"password",bson:"password,omitempty"`
 }
